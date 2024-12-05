80ca7dc1379a233dc76fc916112ed886f33a05d7
josietwirls <128499878+josietwirls@users.noreply.github.com>
2024-12-05 11:36:01 -0600

    Update MotorTest.py

--- MotorTest.py
+++ MotorTest.py
@@ -1,10 +1,10 @@
 import RPi.GPIO as GPIO
 import time
 
-pump1 = 16
-pump2 = 18
-pump3 = 
-pump4 = 
+pump1 = 26
+pump2 = 16
+pump3 = 20
+pump4 = 21
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(pump1, GPIO.OUT)
@@ -26,52 +26,27 @@ try:
             GPIO.output(pump2, True)
             time.sleep(0.1)
             GPIO.output(pump2, False)
-			GPIO.output(pump3, True)
-			time.sleep(0.1)
-			GPIO.output(pump3, False)
-			GPIO.output(pump4, True)
-			time.sleep(0.1)
-			GPIO.output(pump4, False)
+	    GPIO.output(pump3, True)
+	    time.sleep(0.1)
+	    GPIO.output(pump3, False)
+	    GPIO.output(pump4, True)
+	    time.sleep(0.1)
+	    GPIO.output(pump4, False)
 			
-      
-      GPIO.output(pump1,False)
-      GPIO.output(pump2,False)
-	  GPIO.output(pump3,False)
-	  GPIO.output(pump4,False)
-
       for x in range(4):
             GPIO.output(pump1, True)
             time.sleep(0.05)
             GPIO.output(pump1, False)
-            time.sleep(0.05)
-      GPIO.output(pump1,True)
-
-      for x in range(4):
-            GPIO.output(pump2, True)
+	    GPIO.output(pump2, True)
             time.sleep(0.05)
             GPIO.output(pump2, False)
-            time.sleep(0.05)
-      GPIO.output(pump2,True)
-	  
-	  for x in range(4):
             GPIO.output(pump3, True)
             time.sleep(0.05)
             GPIO.output(pump3, False)
-            time.sleep(0.05)
-      GPIO.output(pump3,True)
-	  
-	  for x in range(4):
             GPIO.output(pump4, True)
             time.sleep(0.05)
             GPIO.output(pump4, False)
-            time.sleep(0.05)
-      GPIO.output(pump4,True)
 
-
-	  GPIO.output(pump1,False)
-      GPIO.output(pump2,False)
-	  GPIO.output(pump3,False)
-	  GPIO.output(pump4,False)
 
 
 except KeyboardInterrupt:
