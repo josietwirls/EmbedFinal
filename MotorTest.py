4574777d1c35d17d1ce642adcc2a0e17bacf1ffb
josietwirls <128499878+josietwirls@users.noreply.github.com>
2024-12-09 13:26:55 -0600

    Update MotorTest.py

--- MotorTest.py
+++ MotorTest.py
@@ -3,35 +3,39 @@ import time
 
 pump1 = 26
 pump2 = 16
-pump3 = 20
+pump3 = 19
 pump4 = 21
 
-GPIO.setmode(GPIO.BOARD)
+ON = False
+OFF = True
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
 GPIO.setup(pump1, GPIO.OUT)
 GPIO.setup(pump2, GPIO.OUT)
 GPIO.setup(pump3, GPIO.OUT)
 GPIO.setup(pump4, GPIO.OUT)
 
-GPIO.output(pump1, False)
-GPIO.output(pump2, False)
-GPIO.output(pump3, False)
-GPIO.output(pump4, False)
+GPIO.output(pump1, OFF)
+GPIO.output(pump2, OFF)
+GPIO.output(pump3, OFF)
+GPIO.output(pump4, OFF)
 
 try:
     while True:
-      for x in range(5):
-            GPIO.output(pump1, True)
-            time.sleep(0.1)
-            GPIO.output(pump1, False)
-            GPIO.output(pump2, True)
-            time.sleep(0.1)
-            GPIO.output(pump2, False)
-	    GPIO.output(pump3, True)
-	    time.sleep(0.1)
-	    GPIO.output(pump3, False)
-	    GPIO.output(pump4, True)
-	    time.sleep(0.1)
-	    GPIO.output(pump4, False)
+            GPIO.output(pump1, ON)
+            time.sleep(1)
+            GPIO.output(pump1, OFF)
+            GPIO.output(pump2, ON)
+            time.sleep(1)
+            GPIO.output(pump2, OFF)
+            GPIO.output(pump3, ON)
+            time.sleep(1)
+            GPIO.output(pump3, OFF)
+            GPIO.output(pump4, ON)
+            time.sleep(1)
+            GPIO.output(pump4, OFF)
 			
 
 
